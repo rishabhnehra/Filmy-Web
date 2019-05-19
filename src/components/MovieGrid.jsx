7550bbfc6f728c2541d13f4d503501dedb09a988
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2019-05-19 15:28:21 +0530

    Link to movie details added

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withRouter } from 'react-router'
 import { Grid, Cell, Row } from '@material/react-layout-grid'
 
 import '@material/react-layout-grid/dist/layout-grid.css';
 
-const MovieGrid = ({ data }) => (
-    <Grid>
-        <Row>
-            {data.results && data.results.map(result =>
-                <Cell className="flex flex-center" columns={2}>
-                    <img src={`https://image.tmdb.org/t/p/w300/${result.poster_path}`} alt={result.title} />
-                </Cell>)}
-        </Row>
-    </Grid>
-)
-
-MovieGrid.propTypes = {
-    data: PropTypes.object
+const MovieGrid = ({ data, history }) => {
+    return (
+        <Grid>
+            <Row>
+                {data.results && data.results.map(result =>
+                    <Cell key={result.id} onClick={() => history.push(`/movie/${result.id}`)} className="flex flex-center" columns={2}>
+                        <img src={`https://image.tmdb.org/t/p/w300/${result.poster_path}`} alt={result.title} />
+                    </Cell>)}
+            </Row>
+        </Grid>
+    )
 }
 
-export default MovieGrid
+MovieGrid.propTypes = {
+    data: PropTypes.array.isRequired,
+    history: PropTypes.object.isRequired
+}
+
+export default withRouter(MovieGrid)
