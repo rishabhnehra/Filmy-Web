43928430a0b70fde23e78c8eb46a1b29f09f3fd7
rishabhnehra <rishabhnehrapersonal@gmail.com>
2022-06-28 06:10:57 +0000

    Updates

---
+++ precache-manifest.f8ad2dbb1dec4b8a495e0786fb1c758f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2dd105c2c75fc97ea0b2d91f3529608",
+    "url": "/filmy-web/index.html"
+  },
+  {
+    "revision": "5837be23705e0dd01a49",
+    "url": "/filmy-web/static/css/2.dfe7a5d7.chunk.css"
+  },
+  {
+    "revision": "a4d28a24da29968114d0",
+    "url": "/filmy-web/static/css/main.5ba6027d.chunk.css"
+  },
+  {
+    "revision": "5837be23705e0dd01a49",
+    "url": "/filmy-web/static/js/2.7bc21cbd.chunk.js"
+  },
+  {
+    "revision": "a4d28a24da29968114d0",
+    "url": "/filmy-web/static/js/main.7b207c0b.chunk.js"
+  },
+  {
+    "revision": "3b325419e1ee805f88b7",
+    "url": "/filmy-web/static/js/runtime~main.92434ed2.js"
+  },
+  {
+    "revision": "484df95f22cf8950d5732a2dfef8b032",
+    "url": "/filmy-web/static/media/logo.484df95f.png"
+  }
+]);
